842ea7e556e5ea445e2e8d200f3e5a0d6502cacc
abdulsalamhamandoush22 <abdulsalmhamandoush22@gmail.com>
2023-07-04 16:05:23 +0300

    tags model Fixes #18

---
+++ src/models/Users.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  googleAccountID: String,
+  tel: Number,
+  age: { type: Number, required: true },
+  profile_pic: String,
+  interest_ids: [{ type: Schema.Types.ObjectId, ref: 'Tags' }],
+  password: { type: String, required: true }
+});
+
+module.exports = mongoose.model('User', userSchema);
