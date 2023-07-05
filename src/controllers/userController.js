1205006bcd28bd6aa09356a230cb4eeea6ca814c
Omid Kayhani <o.kayhani@gmail.com>
2023-07-05 17:25:36 +0300

    Fixes issue #15

---
+++ src/controllers/userController.js
@@ -0,0 +1,125 @@
+const User = require('../models/Users');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const sendEmail = require("../utils/email");
+
+exports.getLogin = (req, res, next) => {
+  // Display login page
+  res.render('login');
+};
+
+exports.postLogin = async (req, res, next) => {
+  // Handle login logic
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).send('Invalid email or password.');
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    return res.status(400).send('Invalid email or password.');
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.cookie('jwt', token, { httpOnly: true });
+
+  res.redirect('/dashboard');
+};
+
+exports.postGoogleAuth = async (req, res, next) => {
+  const { idToken } = req.body;
+
+  const ticket = await client.verifyIdToken({
+    idToken,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+
+  const { name, email, picture } = ticket.getPayload();
+
+  let user = await User.findOne({ email });
+
+  if (!user) {
+    user = new User({
+      name,
+      email,
+      profilePic: picture,
+      googleAccountId: ticket.getUserId(),
+    });
+
+    await user.save();
+    
+    // After the user is created, send a confirmation email
+    await sendEmail(
+      user.email, 
+      subject = 'Welcome to Impakt!', 
+      text = 'You have successfully signed up for an Impakt account with your Google account.'
+    );
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.cookie('jwt', token, { httpOnly: true });
+
+  res.redirect('/dashboard');
+};
+
+exports.getSignup = (req, res, next) => {
+  // Display signup page
+  res.render('signup');
+};
+
+exports.postSignup = async (req, res, next) => {
+  const { name, email, password, passwordConfirmation, idToken } = req.body;
+
+  let user = await User.findOne({ email });
+
+  if (user) {
+    return res.status(400).send('User with this email already exists.');
+  }
+
+  if (idToken) {
+    // If an ID token is provided, this is a signup through Google.
+    const ticket = await client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const { name, email, picture } = ticket.getPayload();
+
+    user = new User({
+      name,
+      email,
+      profilePic: picture,
+      googleAccountId: ticket.getUserId(),
+    });
+  } else {
+    // If no ID token is provided, this is a signup with email and password.
+    if (password !== passwordConfirmation) {
+      return res.status(400).send('Passwords do not match.');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  }
+
+  await user.save();
+
+  // After the user is created, send a confirmation email
+  await sendEmail(
+    user.email, 
+    subject = 'Welcome to Impakt!', 
+    text = 'You have successfully signed up for an Impakt account with your Google account.'
+  );
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.cookie('jwt', token, { httpOnly: true });
+
+  res.redirect('/dashboard');
+};
+
