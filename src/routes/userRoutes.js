d2946e827f7814f8c09f6d2b23be8a2122f07dd0
abdulsalam <abdulsalamhamandoush22@gmail.com>
2023-07-06 12:15:21 +0300

    user/{user_id} route Fixes #5

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,4 +6,9 @@ const userController = require('../controllers/userController');
 // Update user profile route
 router.put('/user:id/update', userController.updateUserProfile);
 
+// GET user profile route
+router.get('/users/:id/profile', userController.getUserProfile);
+
+
+
 module.exports = router;
