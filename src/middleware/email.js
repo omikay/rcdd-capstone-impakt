9144aae4fa1033b83eacdf3480aad8b6b5240845
Omid Kayhani <o.kayhani@gmail.com>
2023-07-06 14:33:45 +0000

    Fixes issue #16: fixing some more issues

--- src/middleware/email.js
+++ src/middleware/email.js
@@ -1,10 +1,10 @@
 // utils/email.js
-require("dotenv").config();
-const nodemailer = require("nodemailer");
+require('dotenv').config();
+const nodemailer = require('nodemailer');
 
 // Create a transporter for sending emails
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+  service: 'Gmail',
   auth: {
     user: process.env.MAILER_SERVICE_USER,
     pass: process.env.MAILER_SERVICE_PASS,
@@ -12,21 +12,21 @@ const transporter = nodemailer.createTransport({
 });
 
 // Function to send an email
-const sendEmail = async (to, subject, text) => {
+const sendEmail = async (recepient, sub, msgBody) => {
   try {
     // Define the email options
     const mailOptions = {
       from: process.env.MAILER_SERVICE_USER,
-      to: to,
-      subject: subject,
-      text: text,
+      to: recepient,
+      subject: sub,
+      text: msgBody,
     };
 
     // Send the email
     await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully");
+    console.log('Email sent successfully');
   } catch (error) {
-    console.error("Error sending email:", error);
+    console.error('Error sending email:', error);
   }
 };
 
