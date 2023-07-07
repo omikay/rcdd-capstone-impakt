865309c2036a6f71ee790fb044d0b9d09c45dec9
Omid Kayhani <o.kayhani@gmail.com>
2023-07-07 13:01:55 +0300

    Fixes the pull request issues of issue #9

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,24 @@
-const router = express.Router();
+const express = require('express');
+const gAuth = require('../middleware/gAuth');
 const userController = require('../controllers/userController');
 
+const router = express.Router();
+
 // Route for user signup
-router.post('/signup', userController.postSignup);
+router.post('/api/signup', userController.signup);
 
 // Route for Google OAuth
-router.post('/auth/google', userController.postGoogleAuth);
+router.get('/api/auth/google', gAuth.authenticate('google', { scope: ['profile', 'email'] }));
 
+// // THIS HAS TO BE UPDATED (REDIRECTION ROUTES) // //
+router.get('/api/auth/google/callback', 
+    gAuth.authenticate('google', { failureRedirect: '/api/login', successRedirect: '/dashboard' })
+);
 
 // Update user profile route
-router.put('/user:id/update', userController.updateUserProfile);
+router.put('dashboard/users/:id/update', userController.updateUserProfile);
 
 // GET user profile route
-router.get('/users/:id/profile', userController.getUserProfile);
+router.get('/dashboard/users/:id/profile', userController.getUserProfile);
+
+module.exports = router;
