cf50d3b0048325d44e626a542d6e1179b0743706
Muhammed Hasan <mohammadhasan22003@gmail.com>
2023-07-14 16:09:24 +0300

    new

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -38,20 +38,20 @@ const createEvent = async (req, res) => {
     const message = `Event created successfully: ${event.title}`;
     await sendEmail(event.hostId.email, 'Event Created', message);
 
-    return res
-      .status(201)
-      .json({ message: 'Event created successfully.', event });
+    res.status(201).json({ message: 'Event created successfully.', event });
   } catch (error) {
     console.error('Error creating event:', error);
+    res.status(500).json({ error: 'An error occurred while creating the event.' });
   }
 };
 
 const getAllEvents = async (req, res) => {
   try {
     const events = await Event.find().populate('tags', 'tag_name');
-    return res.json(events);
+    res.json(events);
   } catch (error) {
     console.error('Error getting events:', error);
+    res.status(500).json({ error: 'An error occurred while getting the events.' });
   }
 };
 
@@ -64,9 +64,10 @@ const getEvent = async (req, res) => {
       return res.status(404).json({ error: 'Event not found' });
     }
 
-    return res.json(event);
+    res.json(event);
   } catch (error) {
     console.error('Error getting event:', error);
+    res.status(500).json({ error: 'An error occurred while getting the event.' });
   }
 };
 
@@ -110,19 +111,17 @@ const updateEvent = async (req, res) => {
     const message = `Event updated: ${event.title}`;
     await sendEmail(event.hostId.email, 'Event Updated', message);
 
-    return res.json({ message: 'Event updated successfully.', event });
+    res.json({ message: 'Event updated successfully.', event });
   } catch (error) {
     console.error('Error updating event:', error);
+    res.status(500).json({ error: 'An error occurred while updating the event.' });
   }
 };
 
 const deleteEvent = async (req, res) => {
   try {
     const { eventId } = req.params;
-    const event = await Event.findByIdAndDelete(eventId).populate(
-      'tags',
-      'tag_name'
-    );
+    const event = await Event.findByIdAndDelete(eventId).populate('tags', 'tag_name');
 
     if (!event) {
       return res.status(404).json({ error: 'Event not found' });
@@ -131,51 +130,10 @@ const deleteEvent = async (req, res) => {
     const message = `Event deleted: ${event.title}`;
     await sendEmail(event.hostId.email, 'Event Deleted', message);
 
-    return res.json({ message: 'Event deleted successfully.' });
+    res.json({ message: 'Event deleted successfully.' });
   } catch (error) {
     console.error('Error deleting event:', error);
-  }
-// Update an event
-const updateEvent = async (req, res) => {
-  const { eventId } = req.params;
-  const { title, description, bannerImage } = req.body;
-
-  try {
-    // Find the event in the database
-    const event = await Event.findById(eventId);
-
-    if (!event) {
-      return res.status(404).json({ error: 'Event not found.' });
-    }
-
-    // Update the event with the provided data
-    event.title = title;
-    event.description = description;
-    event.bannerImage = bannerImage;
-    await event.save();
-
-    // Send notification emails to current participants and the event creator
-    const participants = event.participants;
-    const creatorId = event.creator;
-
-    const recipients = [...participants, creatorId];
-    const eventUrl = `//localhost:${process.env.NODE_LOCAL_PORT}/events/${eventId}`;
-
-    for (const recipientId of recipients) {
-      const recipient = await User.findById(recipientId);
-      const recipientEmail = recipient.email;
-
-      await sendEmail(
-        recipientEmail,
-        `Event Updated: ${event.title}`,
-        `The event "${event.title}" has been updated. You can view the event details [here](${eventUrl}).`
-      );
-    }
-
-    return res.status(200).json({ message: 'Event updated successfully.' });
-  } catch (error) {
-    console.error('Error updating event:', error);
-    return res.status(500).json({ error: 'An error occurred while updating the event.' });
+    res.status(500).json({ error: 'An error occurred while deleting the event.' });
   }
 };
 
@@ -185,28 +143,23 @@ const joinEvent = async (req, res) => {
   const userId = req.user.id;
 
   try {
-    // Find the event in the database
     const event = await Event.findById(eventId);
 
     if (!event) {
       return res.status(404).json({ error: 'Event not found.' });
     }
 
-    // Check if the user is already participating in the event
     if (event.participants.includes(userId)) {
       return res.status(400).json({ error: 'User is already participating in the event.' });
     }
 
-    // Check if the event has reached its capacity
     if (event.participants.length >= event.capacity) {
       return res.status(400).json({ error: 'Event has reached its capacity.' });
     }
 
-    // Add the user to the event's participants array
     event.participants.push(userId);
     await event.save();
 
-    // Add the event to the user's events array
     const user = await User.findById(userId);
     user.events.push(eventId);
     await user.save();
@@ -217,16 +170,18 @@ const joinEvent = async (req, res) => {
       `Dear ${user.name}, you have successfully joined the event ${event.title}.`
     );
 
-    return res.status(200).json({ message: 'User joined the event successfully.' });
+    res.status(200).json({ message: 'User joined the event successfully.' });
   } catch (error) {
     console.error('Error joining event:', error);
-    return res.status(500).json({ error: 'An error occurred while joining the event.' });
+    res.status(500).json({ error: 'An error occurred while joining the event.' });
   }
 };
 
-module.exports = { updateEvent, joinEvent,  createEvent,
+module.exports = {
+  createEvent,
   getAllEvents,
   getEvent,
   updateEvent,
-  deleteEvent }
+  deleteEvent,
+  joinEvent,
 };
