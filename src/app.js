7cec5747db31dc67467cf09c529f4cff6f879224
Muhammed Hasan <mohammadhasan22003@gmail.com>
2023-07-17 16:10:05 +0300

    edit app.js file

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const eventRoutes = require('./routes/eventRoutes');
 const connectToMongo = require('./config/db');
 
 const app = express();
-const port = 5007;
+const port = 5050;
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
