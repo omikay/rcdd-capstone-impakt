05f63dec46255876bc40fc2e50bfe4b52eb6a8c8
abdulsalamhamandoush22 <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-07-17 19:45:18 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,10 +23,14 @@ app.use((err, req, res) => {
   // console.error(err.stack);
   res.status(500).json({ error: 'Internal server error' });
 });
-
-app.listen(port, () => {
-  // console.log(`Server listening on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
   connectToMongo();
 });
 
-module.exports = app;
+afterAll(async () => {
+  // Cleanup code to close the server
+  await server.close();
+});
+
+#module.exports = app;
