0845ea5e1c60d51cc058564a54fa39e165dab37e
abdulsalamhamandoush22 <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-07-18 16:51:35 +0300

    Update blogRoutes.js

---
+++ src/routes/blogRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const isAuthorized = require('../middlewares/auth');
+
+const router = express.Router();
+const {
+  createBlog,
+  updateBlog,
+  deleteBlog,
+  getBlogById,
+  getAllBlogs,
+} = require('../controllers/blogController');
+const isAuthorized = require('../middlewares/auth');
+
+// Create a new blog post
+router.post('/blogPost', isAuthorized, createBlog);
+
+// Update a blog post
+router.patch('/blogPosts/:blogPostId/update',isAuthorized, updateBlog);
+
+// Delete a blog post
+router.delete('/blogPosts/:blogPostId',isAuthorized, deleteBlog);
+
+// Get a blog post by ID
+router.get('/blogPosts/:blogPostId',isAuthorized, getBlogById);
+
+// Get all blog posts
+router.get('/blogPosts',isAuthorized, getAllBlogs);
+
+module.exports = router;
