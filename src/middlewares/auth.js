293a3c354fb3b9da51eb14a96246b06c4f4e54f3
Abdulsalam Hamandoush <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-08-16 23:49:13 +0300

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,30 +1,35 @@
-const jwt = require('jsonwebtoken');
+const isAuthorized = (requiredPermissions) => (req, res, next) => {
+  // Check if the request contains a token in the Authorization header.
+  const token = req.headers.authorization;
 
-function isAuthorized(requiredPermissions) {
-  return (req, res, next) => {
-    const token = req.headers.authorization;
+  if (!token) {
+    // No token provided, return an error.
+    return res.status(401).json({ message: 'No token provided.' });
+  }
 
-    if (!token) {
-      return res.status(401).json({ message: 'No token provided.' });
+  // Try to verify the token with the secret key.
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    // If the token is invalid, return an error.
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token.' });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ message: 'Invalid token.' });
-      }
+    // Get the user's permissions from the decoded token.
+    const userPermissions = decoded.permissions || [];
 
-      const userPermissions = decoded.permissions || [];
-      const hasRequiredPermissions = requiredPermissions.every(permission =>
-        userPermissions.includes(permission)
-      );
+    // Check if the user has all of the required permissions.
+    const hasRequiredPermissions = requiredPermissions.every(permission =>
+      userPermissions.includes(permission)
+    );
 
-      if (hasRequiredPermissions) {
-        next();
-      } else {
-        return res.status(403).json({ message: 'Insufficient permissions.' });
-      }
-    });
-  };
-}
+    // If the user has the required permissions, continue with the request.
+    if (hasRequiredPermissions) {
+      next();
+    } else {
+      // The user does not have the required permissions, return an error.
+      return res.status(403).json({ message: 'Insufficient permissions.' });
+    }
+  });
+};
 
 module.exports = isAuthorized;
