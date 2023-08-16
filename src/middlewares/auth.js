a443c30687fc739592e645e12bae13139c35e92a
Abdulsalam Hamandoush <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-08-16 23:34:53 +0300

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,7 +1,4 @@
 const jwt = require('jsonwebtoken');
-const process = require('process');
-
-require('dotenv').config();
 
 function isAuthorized(requiredPermissions) {
   return (req, res, next) => {
@@ -11,22 +8,22 @@ function isAuthorized(requiredPermissions) {
       return res.status(401).json({ message: 'No token provided.' });
     }
 
-    try {
-      const decoded = jwt.verify(token,  process.env.JWT_SECRET );
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Invalid token.' });
+      }
+
       const userPermissions = decoded.permissions || [];
       const hasRequiredPermissions = requiredPermissions.every(permission =>
         userPermissions.includes(permission)
       );
 
       if (hasRequiredPermissions) {
-        // User is authorized, proceed to the next middleware
         next();
       } else {
         return res.status(403).json({ message: 'Insufficient permissions.' });
       }
-    } catch (error) {
-      return res.status(401).json({ message: 'Invalid token.' });
-    }
+    });
   };
 }
 
