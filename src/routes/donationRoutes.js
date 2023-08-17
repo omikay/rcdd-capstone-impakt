245cd8c63619f2f77251ea60c3bba154d0f3e783
Abdulsalam Hamandoush <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-08-17 12:19:37 +0300

    Update donationRoutes.js

--- src/routes/donationRoutes.js
+++ src/routes/donationRoutes.js
@@ -8,13 +8,8 @@ const isAuthorized = require('../middlewares/auth');
 
 const router = express.Router();
 
-// Render the donation form
-router.get('/api/donations', (req, res) => {
-  res.render('makeDonation');
-});
-
 // Make a new donation
-router.post('/user/:id/donate', makeDonation);
+router.post('/donate', makeDonation);
 
 // Get donations by user
 router.get('/user/:id/donations', getUserDonations);
