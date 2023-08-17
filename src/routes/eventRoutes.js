b5d17271aeebd285d0772f23d832bafe4df42dde
Abdulsalam Hamandoush <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-08-17 18:37:00 +0300

    Update eventRoutes.js

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -15,19 +15,19 @@ const {
 const router = express.Router();
 
 // Create an event
-router.post('/events', createEvent);
+router.post('/events', isAuthorized, createEvent);
 
 // leave event
-router.post('/events/:id/leave', leaveEvent)
+router.post('/events/:id/leave', isAuthorized, leaveEvent);
 
 // get events for user
-router.get('/user/:id/events' , getEventsForUser)
+router.get('/user/:id/events', isAuthorized, getEventsForUser);
 
 // Update an event
-router.patch('/events/:eventId/update', updateEvent);
+router.patch('/events/:id/update', isAuthorized, updateEvent);
 
 // User joins an event
-router.post('/events/:eventId/join', joinEvent);
+router.post('/events/:id/join', isAuthorized, joinEvent);
 
 // Get all events
 router.get('/events', getAllEvents);
@@ -36,9 +36,9 @@ router.get('/events', getAllEvents);
 router.get('/events/search', searchEvents);
 
 // Get a specific event
-router.get('events/:eventId', getEvent);
+router.get('/events/:id', getEvent);
 
 // Delete an event
-router.delete('events/:eventId', deleteEvent);
+router.delete('/events/:id', isAuthorized, deleteEvent);
 
 module.exports = router;
